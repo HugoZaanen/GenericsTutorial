069d15ce8235a3e05217dd3272ff80cc67a0d4a7
Huego <hugozaanen@hotmail.com>
2019-03-04 21:10:25 +0100

    Add project files.

---
+++ GenericsTutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
